d08365f483e7f410a1e70d3ce2f57dc7a56827ef
Jacob Hollister <48007098+JacobHollister@users.noreply.github.com>
2022-11-03 16:09:14 +1000

    updated version

--- routes/analyse.js
+++ routes/analyse.js
@@ -5,10 +5,13 @@ const router = express.Router()
 // Utility Imports
 const { analyzeSentiment } = require('../util/sentiment')
 
+// @desc    Returns analysed sentiment data for tweet
+// @route   POST /api/v1/analyse
+// @access  Public
 router.route('/').post((req, res) => {
     const tweet = req.body
+    
     const analysis = analyzeSentiment(tweet.data.text)
-    //console.log(analysis)
     
     res.status(200).json({...tweet, sentiment: analysis})
 })
--- util/sentiment.js
+++ util/sentiment.js
@@ -1,28 +1,39 @@
+// Package imports
 const natural = require('natural');
 const SpellCorrector = require('spelling-corrector');
 const SW = require('stopword');
 const aposToLexForm = require('apos-to-lex-form');
 
+// Initiate Spelling Corrector
 const spellCorrector = new SpellCorrector();
 spellCorrector.loadDictionary();
 
+// Initiate Word Tokenizer
 const { WordTokenizer } = natural;
 const tokenizer = new WordTokenizer();
 
+// Setup sentiment analyzer
 const { SentimentAnalyzer, PorterStemmer } = natural;
 const analyzer = new SentimentAnalyzer('English', PorterStemmer, 'afinn');
 
+// Returns sentiment of a given string of text
 const analyzeSentiment = (text) => {
+    // Remove special characters
     const formattedText = aposToLexForm(text).toLowerCase().replace(/[^a-zA-Z\s]+/g, '')
-    const tokenizedReview = tokenizer.tokenize(formattedText);
+
+    // Tokenize words
+    const tokenizedText = tokenizer.tokenize(formattedText);
     
-    tokenizedReview.forEach((word, index) => {
-        tokenizedReview[index] = spellCorrector.correct(word);
+    // Correct Spelling of each word
+    tokenizedText.forEach((word, index) => {
+        tokenizedText[index] = spellCorrector.correct(word);
     })
     
-    const filteredReview = SW.removeStopwords(tokenizedReview);
+    // Remove stop words
+    const filteredText = SW.removeStopwords(tokenizedText);
     
-    const analysis = analyzer.getSentiment(filteredReview);
+    // Analyse sentiment of correct string
+    const analysis = analyzer.getSentiment(filteredText);
 
     return analysis
 } 
